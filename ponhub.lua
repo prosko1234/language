bfae2742526e2c978e0c123e6849019ed98c4da4
prosko1234 <131814064+prosko1234@users.noreply.github.com>
2023-04-30 12:37:55 +0300

    Create ponhub.lua

---
+++ ponhub.lua
@@ -0,0 +1,18 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("Pon hub(discord.gg/tA3M5gZNdT)", "DarkTheme")
+
+-- MAIN
+local Main = Window:NewTab("Language")
+local MainSection = Main:NewSection("Language")
+
+MainSection:NewButton("English", "language", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/prosko1234/ponhub1/main/ponhub1.lua"))()
+end)
+
+MainSection:NewButton("Українська", "мова", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/prosko1234/ponhub1/main/ponhub1.lua"))()
+end)
+
+MainSection:NewButton("Русский", "язык", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/prosko1234/ponhub1/main/ponhub1.lua"))()
+end)
